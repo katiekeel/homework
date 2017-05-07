df737bd65c0ce8a5d6fdccbbac5ca9695c143364
Katie Shermer <katieshermer@gmail.com>
2017-05-07 10:53:46 -0600

    Uncommented insertion sort; works no w

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,29 +1,26 @@
-      # unsorted_array = [4,1,3,6,9,27,845,2345,5,7,8]
-      #
-      # sorted_array = []
-      #
-      # current_value = unsorted_array.shift
-      #
-      # sorted_array << current_value
-      #
-      # until unsorted_array.empty?
-      #
-      #   insert_index = 0
-      #
-      #   current_value = unsorted_array.shift
-      #
-      #   sorted_array.each_with_index do |item, index|
-      #
-      #     if current_value < item
-      #       insert_index = index
-      #       break
-      #
-      #     else
-      #       insert_index += 1
-      #     end
-      #   end
-      #
-      #   sorted_array.insert(insert_index, current_value)
-      #
-      # end
-      # puts sorted_array
+unsorted_array = [4,1,3,6,9,27,845,2345,5,7,8]
+
+sorted_array = []
+
+current_value = unsorted_array.shift
+
+sorted_array << current_value
+
+until unsorted_array.empty?
+
+insert_index = 0
+
+current_value = unsorted_array.shift
+
+sorted_array.each_with_index do |item, index|
+    if current_value < item
+      insert_index = index
+      break
+    else
+      insert_index += 1
+      end
+    end
+  sorted_array.insert(insert_index, current_value)
+end
+
+puts sorted_array
